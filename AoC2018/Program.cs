bbefee23633dec386fb7f3cd3aaf10dedd561315
kszicsillag <kszicsillag@gmail.com>
2018-12-01 18:16:34 +0100

    Add project files.

---
+++ AoC2018/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AoC2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"1a:{Day1a}");
+            Console.WriteLine($"1b:{Day1b}");
+            Console.ReadLine();
+        }
+       
+
+        public static int Day1a => File.ReadLines("input1.txt").Select(int.Parse).Sum();
+
+        public static int? Day1b => File.ReadLines("input1.txt").Select(int.Parse)
+            .Cycle()
+            .Scan(new {sum = 0, added = true, sumList = new HashSet<int>()}, 
+                (acc, item) =>
+                {
+                    int newsum = acc.sum + item;
+                    bool added = acc.sumList.Add(newsum);
+                    return new {sum = newsum, added, acc.sumList};
+                }
+            )            
+            .First(x=> !x.added).sum;
+
+    }
+}
---
+++ AoC2018/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC2018
+{
+    static class Utils
+    {
+        /*https://codereview.stackexchange.com/a/92070*/
+        public static IEnumerable<T> Cycle<T>(this IEnumerable<T> list, int index = 0)
+        {
+
+            var count = list.Count();
+            index = index % count;
+
+            while (true)
+            {
+                yield return list.ElementAt(index);
+                index = (index + 1) % count;
+            }
+        }
+    }
+}
