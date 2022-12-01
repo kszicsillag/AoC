e8ae7fb17483549d44eef07627b9b26733ca6eda
Gábor Simon <kszicsillag@gmail.com>
2022-12-01 19:37:33 +0000

    day1

--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reactive.Linq;
+
+//Day1a();
+await Day1b();
+
+void Day1a()
+{
+    IObservable<string> fo=File.ReadLines("input/day1.txt").ToObservable().Publish().RefCount();
+    fo.Buffer(fo.Where(l=>l==string.Empty)).Max(lb=>lb.Where(l=>l!=string.Empty).Select(int.Parse).Sum()).Do(Console.WriteLine).Subscribe();
+}
+
+
+async Task Day1b()
+{
+    IObservable<string> fo=File.ReadLines("input/day1.txt").ToObservable().Publish().RefCount();
+    var x=await fo.Buffer(fo.Where(l=>l==string.Empty)).Select(lb=>lb.Where(l=>l!=string.Empty).Select(int.Parse).Sum())
+        .Aggregate(new int[3],(a,x)=>a.Union(new int[]{x}).OrderByDescending(x=>x).Take(3).ToArray()).FirstAsync();
+    System.Console.WriteLine(x.Sum());
+}
