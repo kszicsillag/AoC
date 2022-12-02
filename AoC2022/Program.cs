cf52bbed10702a2e1e8507adc993e820a5ee604a
Gábor Simon <kszicsillag@gmail.com>
2022-12-02 08:38:50 +0000

    day2

--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -2,7 +2,76 @@
 using System.Reactive.Linq;
 
 //Day1a();
-await Day1b();
+//await Day1b();
+Day2b();
+
+
+void Day2a()
+{
+    var x=File.ReadLines("input/day2.txt")
+        .Select(x=>x.Split(' '))
+        .Sum(
+            x=> x.Last() switch
+            {
+                "X" => 1,
+                "Y" => 2,
+                "Z" => 3,
+                _ => 0            
+            }
+            +
+            (x.First(), x.Last()) switch
+            {
+                ("A", "X") => 3,
+                ("B", "Y") => 3,
+                ("C", "Z") => 3,
+                ("A", "Z") => 0,
+                ("B", "X") => 0,
+                ("C", "Y") => 0,
+                ("A", "Y") => 6,
+                ("B", "Z") => 6,
+                ("C", "X") => 6,
+                _ => 0            
+            }
+        );
+        //x.TakeLast(50).ToList().ForEach(System.Console.WriteLine);
+        System.Console.WriteLine(x);
+}
+
+
+void Day2b()
+{
+    var x=File.ReadLines("input/day2.txt")
+        .Select(x=>x.Split(' '))
+        .Sum(
+            x=> x.Last() switch
+            {
+                "X" => 0,
+                "Y" => 3,
+                "Z" => 6,
+                _ => 0                
+            }
+            +
+            (x.First(), x.Last()) switch
+            {
+                ("A", "X") => 3,
+                ("B", "Y") => 2,
+                ("C", "Z") => 1,
+                ("A", "Z") => 2,
+                ("B", "X") => 1,
+                ("C", "Y") => 3,
+                ("A", "Y") => 1,
+                ("B", "Z") => 3,
+                ("C", "X") => 2,
+                _ => 0   
+            }
+        );
+        //x.TakeLast(50).ToList().ForEach(System.Console.WriteLine);
+        System.Console.WriteLine(x);
+}
+
+
+
+
 
 void Day1a()
 {
