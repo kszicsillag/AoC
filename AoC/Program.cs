074cef003667272d16fbc865a5210d3ce14782b3
Gábor Simon <kszicsillag@gmail.com>
2023-12-01 08:16:25 +0000

    v2023init

---
+++ AoC/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Day1a();
+
+void Day1a()
+{
+}
+
+/*
+async Task Day1b()
+{
+    IObservable<string> fo=File.ReadLines("input/day1.txt").ToObservable().Publish().RefCount();
+    var x=await fo.Buffer(fo.Where(l=>l==string.Empty)).Select(lb=>lb.Where(l=>l!=string.Empty).Select(int.Parse).Sum())
+        .Aggregate(new int[3],(a,x)=>a.Union(new int[]{x}).OrderByDescending(x=>x).Take(3).ToArray()).FirstAsync();
+    System.Console.WriteLine(x.Sum());
+}
+*/
